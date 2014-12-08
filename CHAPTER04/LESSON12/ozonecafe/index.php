5a3302413ed0521f6665c80587793c4e73cc3a6f
ishihara_takashi <ishihara_note02@ishihara-note02.local>
2014-12-08 17:11:31 +0900

    meta キーワードとディスクリプションを削除

--- CHAPTER04/LESSON12/ozonecafe/index.php
+++ CHAPTER04/LESSON12/ozonecafe/index.php
@@ -1,291 +1,180 @@
 <!DOCTYPE html>
-<html>
-
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>ブログ記事一覧 | OzoneCafe</title>
-    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-    <meta name="keywords" content="大曽根,カフェ,名古屋,老舗,コーヒーショップ">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <?php wp_head(); ?>
-
-  </head>
-
-  <body class="blog archive">
-
-    <!-- ヘッダー -->
-    <header id="masthead" class="header">
-
-      <div class="container">
-
-        <!-- ロゴ -->
-        <div class="header-logo">
-          <a href="<?php echo site_url(); ?>/index.html">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="OzoneCafe">
-          </a>
-          <!-- /ロゴ -->
-        </div>
-
-        <!-- グローバルナビゲーション -->
-        <nav class="header-navigation">
-
-          <ul class="nav-menu">
-
-            <li>
-              <a href="<?php echo site_url(); ?>/index.html">HOME</a>
-            </li>
-
-            <li>
-              <a href="<?php echo site_url(); ?>/shop/index.html">SHOP</a>
-              <ul class="sub-menu">
-                <li><a href="<?php echo site_url(); ?>/shop/taste.html">こだわり</a>
-                </li>
-                <li><a href="<?php echo site_url(); ?>/shop/history.html">歴史</a>
-                </li>
-              </ul>
-            </li>
-
-            <li>
-              <a href="<?php echo site_url(); ?>/menus/index.html">MENUS</a>
-            </li>
-
-            <li>
-              <a href="<?php echo site_url(); ?>/recruit/index.html">RECRUIT</a>
-              <ul class="sub-menu">
-                <li><a href="<?php echo site_url(); ?>/recruit/form.html">応募フォーム</a>
-                </li>
-              </ul>
-            </li>
-
-            <li>
-              <a href="<?php echo site_url(); ?>/blog/index.html">BLOG</a>
-            </li>
-
-          </ul>
-          <!-- /グローバルナビゲーション -->
-        </nav>
-
-      </div>
-      <!-- /ヘッダーここまで -->
-    </header>
-
-    <div id="main" class="l-two-column">
-
-      <div class="container">
-
-        <!-- メインカラム -->
-        <div id="main-content" class="l-main site-main">
-
-          <div class="content-area posts">
-            <div class="site-content" role="main">
-
-              <section class="post blog-archive">
-
-                <!-- 記事 - ヘッダー -->
-                <header class="entry-header">
-                  <h1 class="entry-title archive-title">
-                    ブログ記事一覧
-                  </h1>
-                </header>
-
-                <!-- 記事 - コンテンツ -->
-                <div class="entry-content">
-                  <article id="post-1" class="post hentry">
-
-                    <header class="entry-header">
-
-                      <h1 class="entry-title"><a href="<?php echo site_url(); ?>/blog/article01.html">サイトをオープンしました。</a>
-                      </h1>
-
-                      <div class="entry-meta">
-
-                        <span class="date"><time class="entry-date">2014年9月22日</time></span>
-
-                        <span class="categories-links info"><a href="#" rel="category">新着情報</a></span>
-                      </div>
-                    </header>
-
-                    <div class="entry-content">
-                      <div class="thumbnail">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/blog01-thumbnail01.png" alt="サイト公開しました。">
-                      </div>
-
-                      <p>2015年2月10日に本Webサイトをオープンしました。
-                        <br> WordCafe 最新情報、お役立ち情報を随時掲載していきます。</p>
-                      </p>
-
-                    </div>
-
-                  </article>
-
-                  <article id="post-2" class="post hentry">
-
-                    <header class="entry-header">
-
-                      <h1 class="entry-title"><a href="<?php echo site_url(); ?>/">季節のショートケーキを発売します。</a>
-                      </h1>
-
-                      <div class="entry-meta">
-
-                        <span class="date"><time class="entry-date">2014年9月22日</time></span>
-
-                        <span class="categories-links limited"><a href="#" rel="category">期間限定</a></span>
-                      </div>
-
-                    </header>
-
-                    <div class="entry-content">
-                      <div class="thumbnail">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/menu08-thumbnail01.png" alt="季節のショートケーキ">
-                      </div>
-
-                      <p>
-                        2015年3月10日から4月10日までの1ヶ月間限定として、季節のショートケーキを販売致します。
-                        <br> 通常のチーズケーキより、約2倍チーズを使用しお作りするスベシャルメニューです。
-                      </p>
-
-                    </div>
-
-                  </article>
-
-                  <nav class="navigation paging-navigation" role="navigation">
-                    <div class="nav-links">
-                      <div class="nav-previous">
-                        <a href="<?php echo site_url(); ?>/"><span class="meta-nav">←</span> 古い投稿</a>
-                      </div>
-                      <div class="nav-next">
-                        <a href="<?php echo site_url(); ?>/">新しい投稿 <span class="meta-nav">→</span></a>
-                      </div>
-                    </div>
-                  </nav>
-
-                </div>
-
-              </section>
-            </div>
-          </div>
-
-          <!-- /メインカラム -->
-        </div>
-
-        <!-- サイドバー -->
-        <aside id="secondary" class="l-sidebar">
-
-          <!-- サイドバー - 検索 -->
-          <div class="widget widget_search">
-            <div class="search form-control">
-              <input type="search" name="s" id="s" class="search-input" value="">
-              <input type="submit" value="検索" class="search-submit">
-            </div>
-          </div>
-
-          <!-- サイドバー - バナー -->
-          <div class="banner">
-
-            <p>
-              <a href="<?php echo site_url(); ?>/recruit/form.html">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/banner-contact.png" alt="お問い合わせ">
-              </a>
-            </p>
-
-            <p>
-              <a href="<?php echo site_url(); ?>/shop/index.html#access">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/banner-access.png" alt="アクセス">
-              </a>
-            </p>
-
-            <p>
-              <a href="<?php echo site_url(); ?>/blog/index.html">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/banner-blog.png" alt="ブログ">
-              </a>
-            </p>
-
-          </div>
-
-          <!-- サイドバー - カテゴリー -->
-          <div class="widget widget_category">
-
-            <h2 class="widget-title">
-              メニューカテゴリー
-            </h2>
-
-            <ul>
-              <li>
-                <a href="<?php echo site_url(); ?>/menu/index.html">アイスドリンク</a>
-              </li>
-              <li>
-                <a href="<?php echo site_url(); ?>/menu/index.html">ホットドリンク</a>
-              </li>
-              <li>
-                <a href="<?php echo site_url(); ?>/menu/index.html">フード</a>
-              </li>
-              <li>
-                <a href="<?php echo site_url(); ?>/menu/index.html">スイーツ</a>
-              </li>
-            </ul>
-
-          </div>
-
-          <!-- /サイドバー -->
-        </aside>
-
-      </div>
-
-    </div>
-
-    <!-- フッター -->
-    <footer id="colophon" class="footer text-center">
-
-      <div class="container">
-
-        <div class="footer-logo">
-          <a href="<?php echo site_url(); ?>/index.html">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" alt="OzoneCafe">
-          </a>
-          <p>"一杯"に込める思い。OzoneCafe</p>
-        </div>
-
-        <div class="footer-navigation">
-          <ul>
-            <li>
-              <a href="<?php echo site_url(); ?>/">トップページ</a>
-            </li>
-            <li>
-              <a href="<?php echo site_url(); ?>/shop/index.html">店舗案内</a>
-            </li>
-            <li>
-              <a href="<?php echo site_url(); ?>/menus/index.html">メニュー</a>
-            </li>
-            <li>
-              <a href="<?php echo site_url(); ?>/blog/index.html">ブログ</a>
-            </li>
-            <li>
-              <a href="<?php echo site_url(); ?>/recruit/index.html">スタッフ募集</a>
-            </li>
-            <li>
-              <a href="<?php echo site_url(); ?>/recruit/form.html">求人応募</a>
-            </li>
-            <li>
-              <a href="<?php echo site_url(); ?>/sitemap.html">サイトマップ</a>
-            </li>
-
-          </ul>
-        </div>
-
-        <div class="footer-copyright">
-          <p>copyright © 2014 Ozone Cafe All Right Reserved.</p>
-        </div>
-
-      </div>
-      <!-- /フッターここまで -->
-    </footer>
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-
-    <?php wp_footer(); ?>
-
-  </body>
+<html <?php language_attributes(); ?>>
+
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+		<meta name="description" content="60年以上続くこの味を。名古屋市大曽根で楽しめる、本格的コーヒーショップ">
+		<meta name="keywords" content="大曽根,カフェ,名古屋,老舗,コーヒーショップ">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="style.css" />
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<!-- ▼ヘッダー : 開始 -->
+		<header class="header">
+
+			<div class="container">
+
+				<div class="header-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/common/logo.png" alt="OzoneCafe">
+					</a>
+				</div>
+				<!--/.header-logo-->
+
+				<nav class="header-navigation">
+
+					<ul class="nav-menu">
+
+						<li>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">HOME</a>
+						</li>
+
+						<li>
+							<span>SHOP</span>
+							<ul class="sub-menu">
+								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>">ショップについて</a>
+								</li>
+								<li><a href="<?php echo esc_url( home_url( '/shop/taste.html' ) ); ?>">こだわり</a>
+								</li>
+								<li><a href="<?php echo esc_url( home_url( '/shop/history.html' ) ); ?>">歴史</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="<?php echo esc_url( home_url( '/menus/' ) ); ?>">MENUS</a>
+						</li>
+
+						<li>
+							<span>RECRUIT</span>
+							<ul class="sub-menu">
+								<li><a href="<?php echo esc_url( home_url( '/recruit/' ) ); ?>">スタッフ募集</a>
+								</li>
+								<li><a href="<?php echo esc_url( home_url( '/recruit/form.html' ) ); ?>">応募フォーム</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="<?php echo esc_url( home_url( '/blog/' ) ); ?>">BLOG</a>
+						</li>
+
+					</ul>
+				</nav>
+				<!--/.header-navigation-->
+
+			</div>
+
+		</header>
+		<!-- ▲ヘッダー : 終了 -->
+
+		<!--▼トップページ メインビジュアル : 開始-->
+		<div class="visual">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/main-visual.png" alt="いっぱいに込める思い。OzoneCafe">
+			<div class="container">
+				<h1 class="site-desctiption">
+					"一杯"に込める思い。OzoneCafe
+				</h1>
+			</div>
+		</div>
+		<!--▲トップページ メインビジュアル : 終了-->
+
+		<!--▼メインコンテンツ 開始-->
+		<div id="main" class="l-one-column clearfix">
+
+			<div class="container">
+
+				<div class="l-main" role="main">
+
+					<section class="home posts">
+
+						<div class="home-banner">
+							<ul>
+								<li>
+									<a href="<?php echo esc_url( home_url( '/menus/' ) ); ?>">
+										<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/home-banner-menu.png" alt="メニュー">
+									</a>
+								</li>
+								<li>
+									<a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>">
+										<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/home-banner-access.png" alt="アクセス">
+									</a>
+								</li>
+								<li>
+									<a href="<?php echo esc_url( home_url( '/recruit/' ) ); ?>">
+										<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/home-banner-recruit.png" alt="スタッフ募集">
+									</a>
+								</li>
+							</ul>
+						</div>
+
+					</section>
+
+				</div>
+				<!-- /.l-main -->
+
+			</div>
+
+		</div>
+		<!--▲メインコンテンツ 終了-->
+
+		<!--▼フッター : 開始-->
+		<footer id="colophon" class="footer text-center">
+
+			<div class="container">
+
+				<div class="footer-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/common/logo-footer.png" alt="OzoneCafe">
+					</a>
+					<p><?php bloginfo( 'description' ); ?></p>
+				</div>
+				<!--/.footer-logo-->
+
+				<div class="footer-navigation">
+					<ul>
+						<li>
+							<a href="">トップページ</a>
+						</li>
+						<li>
+							<a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>">店舗案内</a>
+						</li>
+						<li>
+							<a href="<?php echo esc_url( home_url( '/menus/' ) ); ?>">メニュー</a>
+						</li>
+						<li>
+							<a href="<?php echo esc_url( home_url( '/blog/' ) ); ?>">ブログ</a>
+						</li>
+						<li>
+							<a href="<?php echo esc_url( home_url( '/recruit/' ) ); ?>">スタッフ募集</a>
+						</li>
+						<li>
+							<a href="<?php echo esc_url( home_url( '/recruit/form.html' ) ); ?>">求人応募</a>
+						</li>
+						<li>
+							<a href="<?php echo esc_url( home_url( '/sitemap.html' ) ); ?>">サイトマップ</a>
+						</li>
+					</ul>
+
+				</div>
+				<!--/.footer-navigation-->
+
+				<div class="footer-copyright">
+					<p>copyright © 2014 <?php bloginfo( 'name' ); ?> All Right Reserved.</p>
+				</div>
+
+			</div>
+
+		</footer>
+		<!--▲フッター : 終了-->
+
+
+		<?php wp_footer(); ?>
+	</body>
 
 </html>
