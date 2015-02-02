478a128df205e87045413454970eaef66b00e584
ishihara <ishihara_note02@ishihara-note02.local>
2015-02-02 20:28:54 +0900

    lesson20まで原稿に合わせ修正

---
+++ chapter06/lesson20/after/ozonecafe/functions.php
@@ -0,0 +1,41 @@
+<?php
+//コンテンツ幅をセット
+if ( ! isset( $content_width ) ) {
+	$content_width = 726;
+}
+
+function custom_theme_setup() {
+	//head内にフィードリンクを出力
+	add_theme_support( 'automatic-feed-links' );
+
+	//タイトルタグを動的に出力
+	add_theme_support( 'title-tag' );
+
+	//アイキャッチ画像を有効化
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 175, 175, false );
+
+	// カスタムメニューを有効化
+	add_theme_support( 'menus' );
+	// テーマの位置を定義
+	register_nav_menus(
+		array(
+			'header' => 'グローバルナビゲーション',
+			'footer' => 'フッターナビゲーション',
+		)
+	);
+
+}
+add_action( 'after_setup_theme', 'custom_theme_setup' );
+
+//スタイルシート読み込み
+function add_my_scripts() {
+	wp_enqueue_style(
+		'base-style', //CSSの識別ID
+		esc_url( get_stylesheet_uri() ), //CSSファイルへのpath ←追記
+		array(), //先に読み込むCSS
+		'1.0', //CSSファイルのバージョン指定
+		'all' //CSSのmedia属性
+	);
+}
+add_action( 'wp_enqueue_scripts', 'add_my_scripts' );
---
+++ chapter06/lesson20/after/ozonecafe/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- ▼ヘッダー : 開始-->
+		<header class="header">
+			<div class="container">
+				<div class="header-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ) ?>"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/logo.png" alt="Ozone Cafe"></a>
+				</div><!--/.header-logo-->
+
+				<nav class="header-navigation">
+
+					<?php
+					/**
+					* グローバルナビゲーション
+					*/
+					wp_nav_menu( array(
+						'theme_location' => 'header',
+					) );
+					?>
+				</nav><!--/.header-navigation-->
+
+			</div>
+		</header>
+		<!-- ▲ヘッダー : 終了 -->
